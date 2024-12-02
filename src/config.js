b8c999f78f517179290b0cf06fcf769d7f1c8d32
MauricioSuxoAndrade <suxoandree@gmail.com>
2024-12-02 16:53:41 -0400

    LePrueba

---
+++ src/config.js
@@ -0,0 +1,25 @@
+export const MAP_STYLE = "./map_style.json";
+
+export const INITIAL_VIEW_STATE = {
+    longitude: -16.504,
+    latitude:  -68.131   ,
+    zoom: 13,
+    pitch: 0,
+    bearing: 0
+};
+
+export const INITIAL_COLORS = {
+    startNodeFill: [70, 183, 128],
+    startNodeBorder: [255, 255, 255],
+    endNodeFill: [152, 4, 12],
+    endNodeBorder: [0, 0, 0],
+    path: [70, 183, 128],
+    route: [165, 13, 32],
+};
+
+export const LOCATIONS = [
+    { name: "La Paz", latitude: -16.504, longitude: -68.131 },
+    { name: "Viacha", latitude: -16.650, longitude: -68.297 },
+    { name: "Sta. Cruz", latitude: -17.783, longitude: -63.182 }
+    
+];
