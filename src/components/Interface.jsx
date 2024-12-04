71045a0ee8abf999756082b09e5bbe52ad9af31a
MauricioSuxoAndrade <suxoandree@gmail.com>
2024-12-04 03:00:30 -0400

    nodos

--- src/components/Interface.jsx
+++ src/components/Interface.jsx
@@ -173,7 +173,7 @@ const Interface = forwardRef(({ canStart, started, animationEnded, playbackOn, t
 
             <div className="mobile-controls">
                 <Button onClick={() => {setPlaceEnd(!placeEnd);}} style={{ color: "#fff", backgroundColor: "#404156", paddingInline: 30, paddingBlock: 7 }} variant="contained">
-                    {placeEnd ? "Nodo final" : "Nodo inicial"}
+                    {placeEnd ? "Cambiar a nodo inicial" : "Cambiar a nodo final"}
                 </Button>
             </div>
 
